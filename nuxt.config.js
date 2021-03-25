10c73b60d32da35f122962319be8eebe06fec318
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-03-26 01:32:16 +0300

    implements navbar

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,7 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ['@/assets/main.css'],
-  tailwindcss: {
-    darkMode: false,
-  },
+  tailwindcss: {},
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
