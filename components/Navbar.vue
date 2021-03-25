d7c0c74c68ef8969b8b7a0c620ebb0b886793fb0
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-03-26 02:16:50 +0300

    refactors favicon and title

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -131,7 +131,7 @@
         </ul>
         <button
           id="navAction"
-          class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+          class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-3 px-6 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
         >
           Stake Now
         </button>
