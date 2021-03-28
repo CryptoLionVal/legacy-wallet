1222b5c1892bb450dcb629b3c7633e5befe08b97
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-03-29 00:56:00 +0300

    enables callapsing on faq elements

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -2,19 +2,22 @@
   <div class="flex flex-wrap pb-4">
     <div class="w-full font-bold text-xl text-gray-800 px-6">
       {{ title }}
-      <svg
-        class="float-right text-xl"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="24"
-        height="24"
-      >
-        <path
-          d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
-        />
-      </svg>
+      <button class="float-right text-xl" @click="readable = !readable">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class=""
+          :class="{ 'transform rotate-180': readable }"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path
+            d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
+          />
+        </svg>
+      </button>
     </div>
-    <p class="text-gray-800 text-base px-6 mb-5">
+    <p v-if="readable" class="text-gray-800 text-base px-6 mb-5">
       <slot></slot>
     </p>
 
@@ -33,6 +36,11 @@ export default {
       type: String,
       default: '',
     },
+  },
+  data() {
+    return {
+      readable: false,
+    }
   },
 }
 </script>
