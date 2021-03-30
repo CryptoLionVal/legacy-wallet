ac20d3cc55ab8e18b10af81ed0b772c766c94c48
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-03-30 03:56:43 +0300

    implements staking steps

--- pages/how-to-stake-cro.vue
+++ pages/how-to-stake-cro.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section id="delegating-steps" class="bg-white border-b py-10">
+    <section id="delegating-steps" class="bg-white border-b py-10 px-4">
       <div class="container max-w-5xl mx-auto m-8">
         <h1
           class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800"
@@ -12,8 +12,25 @@
             class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"
           ></div>
         </div>
+        <div class="w-full mb-4">
+          <div
+            class="flex flex-wrap md:flex-no-wrap space-y-2 md:space-y-0 md:space-x-5 content-between"
+          >
+            <step-card
+              v-for="step in $t('pages.how_to_stake_cro.steps')"
+              :key="step.title"
+              :title="step.title"
+              :icon="step.icon"
+              :desc="step.desc"
+            />
+          </div>
+        </div>
         <div>
-          <img alt="How to stake CRO" src="@/assets/images/staking.gif" />
+          <img
+            class="rounded"
+            alt="How to stake CRO"
+            src="@/assets/images/staking.gif"
+          />
         </div>
       </div>
     </section>
