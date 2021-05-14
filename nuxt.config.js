21ad371ed1d09a167cb4fd00bab646556341af7c
Mevlüt Canvar <mevlutcanvar@gmail.com>
2021-05-14 19:21:21 +0300

    enjoy, enables vaccined google analytics as plugin this time

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,5 +90,11 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extend(config, { isDev, isClient }) {
+      if (!isDev && isClient) {
+        config.plugins.push({ src: '@/plugins/gtag', ssr: false })
+      }
+    },
+  },
 }
